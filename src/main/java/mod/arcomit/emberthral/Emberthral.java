f10e8da15643c5d04e37c83ce6f7a07d4194f8c8
Arcomit <8089341@qq.com>
2025-03-05 00:42:11 +0800

    Fix the sample code.

--- src/main/java/mod/arcomit/emberthral/Emberthral.java
+++ src/main/java/mod/arcomit/emberthral/Emberthral.java
@@ -38,7 +38,7 @@ public class Emberthral {
     public Emberthral() {
         LOGGER.info("Emberthral is loaded!");
         //示例-仅在开发环境生效
-        if (FMLEnvironment.production) {
+        if (!FMLEnvironment.production) {
             Filter test = new Filter("test",new ItemStack(Items.IRON_INGOT));
             test.accept(Items.IRON_INGOT);
 
@@ -54,6 +54,23 @@ public class Emberthral {
             test5.accept(Blocks.MAGENTA_BED);
 
             FilterManager.registerTabFilters(CreativeModeTabs.BUILDING_BLOCKS,test,test2,test3,test4,test5);
+
+            FilterManager.registerTabFilters(CreativeModeTabs.FOOD_AND_DRINKS,test,test2,test3,test4,test5);
+
+            Filter test6 = new Filter("test",new ItemStack(Items.IRON_INGOT));
+            test6.accept(Items.IRON_INGOT);
+
+            Filter test7 = new Filter("test2",new ItemStack(Items.IRON_INGOT));
+
+            Filter test8 = new Filter("test3",new ItemStack(Items.GOLD_INGOT));
+            test8.accept(Items.GOLD_INGOT);
+
+            Filter test9 = new Filter("test4",new ItemStack(Items.DIAMOND));
+            test9.accept(Items.DIAMOND);
+
+            Filter test10 = new Filter("test5",new ItemStack(Items.MAGENTA_BED));
+            test10.accept(Blocks.MAGENTA_BED);
+            FilterManager.registerTabFilters(CreativeModeTabs.FUNCTIONAL_BLOCKS,test6,test7,test8,test9,test10);
         }
     }
 }
